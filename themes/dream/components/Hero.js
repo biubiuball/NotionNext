601c4c978922ed41f9392f45ef47d01d6de58468
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-05-28 02:48:41 +0800

    Add files via upload

--- themes/dream/components/Hero.js
+++ themes/dream/components/Hero.js
@@ -5,26 +5,21 @@ import { useGlobal } from '@/lib/global'
 import { loadExternalResource } from '@/lib/utils'
 import { useEffect, useState } from 'react'
 import CONFIG from '../config'
-import NavButtonGroup from './NavButtonGroup'
 
 let wrapperTop = 0
 
 /**
- * 顶部全屏大图
- * @returns
+ * 首页英雄区
+ * 是一张大图，带个居中按钮
+ * @returns 头图
  */
 const Hero = props => {
   const [typed, changeType] = useState()
   const { siteInfo } = props
   const { locale } = useGlobal()
-  const scrollToWrapper = () => {
-    window.scrollTo({ top: wrapperTop, behavior: 'smooth' })
-  }
-
   const GREETING_WORDS = siteConfig('GREETING_WORDS').split(',')
   useEffect(() => {
     updateHeaderHeight()
-
     if (!typed && window && document.getElementById('typed')) {
       loadExternalResource('/js/typed.min.js', 'js').then(() => {
         if (window.Typed) {
@@ -46,7 +41,7 @@ const Hero = props => {
     return () => {
       window.removeEventListener('resize', updateHeaderHeight)
     }
-  })
+  }, [])
 
   function updateHeaderHeight() {
     requestAnimationFrame(() => {
@@ -59,39 +54,53 @@ const Hero = props => {
     <header
       id='header'
       style={{ zIndex: 1 }}
-      className='w-full h-screen relative bg-black'>
-      <div className='text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full '>
+      className=' w-full h-screen relative bg-black'>
+      <div className='text-white absolute flex flex-col h-full items-center justify-center w-full '>
         {/* 站点标题 */}
-        <div className='font-black text-4xl md:text-5xl shadow-text'>
+        <div className='text-4xl md:text-5xl shadow-text'>
           {siteInfo?.title || siteConfig('TITLE')}
         </div>
         {/* 站点欢迎语 */}
-        <div className='mt-2 h-12 items-center text-center font-medium shadow-text text-lg'>
+        <div className='mt-2 h-12 items-center text-center shadow-text text-white text-lg'>
           <span id='typed' />
         </div>
-
-        {/* 首页导航大按钮 */}
-        {siteConfig('HEXO_HOME_NAV_BUTTONS', null, CONFIG) && (
-          <NavButtonGroup {...props} />
-        )}
-
         {/* 滚动按钮 */}
         <div
-          onClick={scrollToWrapper}
-          className='z-10 cursor-pointer w-full text-center py-4 text-3xl absolute bottom-10 text-white'>
-          <div className='opacity-70 animate-bounce text-xs'>
+          onClick={() => {
+            window.scrollTo({ top: wrapperTop, behavior: 'smooth' })
+          }}
+          className='glassmorphism mt-12 border cursor-pointer w-40 text-center pt-4 pb-3 text-md text-white hover:bg-[#928cee] duration-300 rounded-3xl z-40'>
+         <style jsx>{`
+  @keyframes my-bounce {
+  0%, 100% {
+    transform: translateY(-25%);
+    animation-timing-function: cubic-bezier(0.8,0,1,1);
+  }
+  50% {
+    transform: translateY(0);
+    animation-timing-function: cubic-bezier(0,0,0.2,1);
+  }
+}
+
+.custom-bounce {
+  animation: my-bounce 1s infinite;
+}
+`}</style>
+
+<i className='custom-bounce fas fa-angle-double-down'></i>
+          
+          <span>
             {siteConfig('HEXO_SHOW_START_READING', null, CONFIG) &&
               locale.COMMON.START_READING}
-          </div>
-          <i className='opacity-70 animate-bounce fas fa-angle-down' />
+          </span>
         </div>
       </div>
 
       <LazyImage
+        priority={true}
         id='header-cover'
-        alt={siteInfo?.title}
         src={siteInfo?.pageCover}
-        className={`header-cover w-full h-screen object-cover object-center ${siteConfig('HEXO_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`}
+        className={`header-cover object-center w-full h-screen object-cover ${siteConfig('HEXO_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`}
       />
     </header>
   )
