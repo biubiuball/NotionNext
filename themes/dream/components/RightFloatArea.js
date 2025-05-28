f6416318208ace52a74dd1b432d336ba3d570c68
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-05-29 04:10:31 +0800

    Update RightFloatArea.js

--- themes/dream/components/RightFloatArea.js
+++ themes/dream/components/RightFloatArea.js
@@ -1,49 +1,20 @@
-import throttle from 'lodash.throttle'
-import { useCallback, useEffect, useState } from 'react'
-import ButtonDarkModeFloat from './ButtonFloatDarkMode'
 import ButtonJumpToTop from './ButtonJumpToTop'
+import ButtonDarkModeFloat from './ButtonDarkModeFloat'
 
 /**
- * 悬浮在右下角的按钮，当页面向下滚动100px时会出现
- * @param {*} param0
+ * 右下角悬浮按钮
+ * @param {*} props
  * @returns
  */
-export default function RightFloatArea({ floatSlot }) {
-  const [showFloatButton, switchShow] = useState(false)
-  const scrollListener = useCallback(
-    throttle(() => {
-      const targetRef = document.getElementById('wrapper')
-      const clientHeight = targetRef?.clientHeight
-      const scrollY = window.pageYOffset
-      const fullHeight = clientHeight - window.outerHeight
-      let per = parseFloat(((scrollY / fullHeight) * 100).toFixed(0))
-      if (per > 100) per = 100
-      const shouldShow = scrollY > 100 && per > 0
-
-      // 右下角显示悬浮按钮
-      if (shouldShow !== showFloatButton) {
-        switchShow(shouldShow)
-      }
-    }, 200)
-  )
-
-  useEffect(() => {
-    document.addEventListener('scroll', scrollListener)
-    return () => document.removeEventListener('scroll', scrollListener)
-  }, [])
-
+export default function RightFloatArea(props) {
+  const { floatSlot } = props
   return (
-    <div
-      className={
-        (showFloatButton ? 'opacity-100 ' : 'invisible opacity-0') +
-        '  duration-300 transition-all bottom-12 right-1 fixed justify-end z-20  text-white bg-indigo-500 dark:bg-hexo-black-gray rounded-sm'
-      }>
-      <div
-        className={'justify-center  flex flex-col items-center cursor-pointer'}>
-        <ButtonDarkModeFloat />
-        {floatSlot}
-        <ButtonJumpToTop />
-      </div>
+    <div className="bottom-40 right-2 fixed flex flex-col items-end space-y-2 z-20">
+      <ButtonDarkModeFloat />
+      <ButtonJumpToTop />
+      
+      {/* 可扩展的右下角悬浮 */}
+      {floatSlot}
     </div>
   )
 }
