146cc6e7856023747cc0e63ff5cd82aac96c36ad
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-05-29 04:10:00 +0800

    Update ButtonJumpToTop.js

--- themes/dream/components/ButtonJumpToTop.js
+++ themes/dream/components/ButtonJumpToTop.js
@@ -1,6 +1,6 @@
-import { siteConfig } from '@/lib/config'
 import { useGlobal } from '@/lib/global'
 import CONFIG from '../config'
+import { siteConfig } from '@/lib/config'
 
 /**
  * 跳转到网页顶部
@@ -16,10 +16,14 @@ const ButtonJumpToTop = ({ showPercent = true, percent }) => {
   if (!siteConfig('HEXO_WIDGET_TO_TOP', null, CONFIG)) {
     return <></>
   }
-  return (<div className='space-x-1 items-center justify-center transform hover:scale-105 duration-200 w-7 h-auto pb-1 text-center' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} >
-        <div title={locale.POST.TOP} ><i className='fas fa-arrow-up text-xs' /></div>
-        {showPercent && (<div className='text-xs hidden lg:block'>{percent}</div>)}
-    </div>)
+
+  return <div data-aos="fade-left"
+        data-aos-duration="300"
+        data-aos-anchor-placement="top-center"
+        className={'justify-center items-center text-center'} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} >
+        <i id="darkModeButton" title={locale.POST.TOP} className={`fas fa-arrow-up transform hover:scale-105 duration-200 text-white
+        bg-indigo-700 w-10 h-10 rounded-full dark:bg-black cursor-pointer py-2.5`} />
+    </div>
 }
 
 export default ButtonJumpToTop
