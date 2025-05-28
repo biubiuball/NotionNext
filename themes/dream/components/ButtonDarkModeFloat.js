1677a8db02177b6e98e5c56172292d53c3018be8
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-05-29 04:09:21 +0800

    Update and rename ButtonFloatDarkMode.js to ButtonDarkModeFloat.js

--- themes/dream/components/ButtonDarkModeFloat.js
+++ themes/dream/components/ButtonDarkModeFloat.js
@@ -23,16 +23,11 @@ export default function ButtonDarkModeFloat() {
     htmlElement.classList?.add(newStatus ? 'dark' : 'light')
   }
 
-  return (
-    <div
-      onClick={handleChangeDarkMode}
-      className={
-        'justify-center items-center w-7 h-7 text-center transform hover:scale-105 duration-200'
-      }>
-      <i
-        id='darkModeButton'
-        className={`${isDarkMode ? 'fa-sun' : 'fa-moon'} fas text-xs`}
-      />
-    </div>
+
+   return (
+        <div className={'justify-center items-center text-center' } onClick={handleChangeDarkMode}>
+            <i id="darkModeButton" className={`${isDarkMode ? 'fa-sun' : 'fa-moon'} fas transform hover:scale-105 duration-200
+                 text-white bg-indigo-700  w-10 h-10  py-2.5 rounded-full dark:bg-black cursor-pointer`} />
+        </div>
   )
 }
