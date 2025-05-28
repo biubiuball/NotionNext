298a3e6aca11bf3ac0be65806b8553b4f3f974ed
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-05-29 04:11:47 +0800

    Update ButtonJumpToComment.js

--- themes/dream/components/ButtonJumpToComment.js
+++ themes/dream/components/ButtonJumpToComment.js
@@ -1,5 +1,5 @@
-import { siteConfig } from '@/lib/config'
 import CONFIG from '../config'
+import { siteConfig } from '@/lib/config'
 
 /**
  * 跳转到评论区
@@ -13,24 +13,14 @@ const ButtonJumpToComment = () => {
 
   function navToComment() {
     if (document.getElementById('comment')) {
-      window.scrollTo({
-        top: document.getElementById('comment').offsetTop,
-        behavior: 'smooth'
-      })
+      window.scrollTo({ top: document.getElementById('comment').offsetTop, behavior: 'smooth' })
     }
-    // 兼容性不好
-    // const commentElement = document.getElementById('comment')
-    // if (commentElement) {
-    // commentElement?.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
   }
 
-  return (
-    <div
-      className='flex space-x-1 items-center justify-center transform hover:scale-105 duration-200 w-7 h-7 text-center'
-      onClick={navToComment}>
-      <i className='fas fa-comment text-xs' />
+  return <div className={'justify-center items-center text-center'} onClick={navToComment}>
+        <i id="darkModeButton" className={`fas fa-comments transform hover:scale-105 duration-200 text-white
+         text-sm  bg-indigo-700 w-10 h-10 rounded-full dark:bg-black cursor-pointer py-3`} />
     </div>
-  )
 }
 
 export default ButtonJumpToComment
