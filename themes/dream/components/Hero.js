d0f2696f2616784634b9af7312c8ec5b29a42214
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-06-13 04:19:43 +0800

    Update Hero.js

--- themes/dream/components/Hero.js
+++ themes/dream/components/Hero.js
@@ -74,7 +74,7 @@ const Hero = props => {
             0%,100% { transform: translateY(-25%); animation-timing-function: cubic-bezier(0.8,0,1,1); }
             50% { transform: translateY(0); animation-timing-function: cubic-bezier(0,0,0.2,1); }
           }
-          .bounce { animation: bounce 1s infinite; }
+          .bounce { animation: bounce 3s infinite; }
   `       }</style>
           <i className='fas fa-angle-double-down bounce' />
           <span>
