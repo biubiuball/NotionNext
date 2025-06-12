5ba72c78c63640d407b51704cbc274c488c2bfef
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-06-13 03:40:57 +0800

    Update Hero.js

--- themes/dream/components/Hero.js
+++ themes/dream/components/Hero.js
@@ -1,4 +1,3 @@
-// import Image from 'next/image'
 import LazyImage from '@/components/LazyImage'
 import { siteConfig } from '@/lib/config'
 import { useGlobal } from '@/lib/global'
@@ -54,8 +53,8 @@ const Hero = props => {
     <header
       id='header'
       style={{ zIndex: 1 }}
-      className=' w-full h-screen relative bg-black'>
-      <div className='text-white absolute flex flex-col h-full items-center justify-center w-full '>
+      className='w-full h-screen relative'>
+      <div className='text-white absolute flex flex-col h-full items-center justify-center w-full'>
         {/* 站点标题 */}
         <div className='text-4xl md:text-5xl shadow-text'>
           {siteInfo?.title || siteConfig('TITLE')}
@@ -70,25 +69,7 @@ const Hero = props => {
             window.scrollTo({ top: wrapperTop, behavior: 'smooth' })
           }}
           className='glassmorphism mt-12 border cursor-pointer w-40 text-center pt-4 pb-3 text-md text-white hover:bg-[#4338ca] duration-300 rounded-3xl z-40'>
-         <style jsx>{`
-  @keyframes my-bounce {
-  0%, 100% {
-    transform: translateY(-25%);
-    animation-timing-function: cubic-bezier(0.8,0,1,1);
-  }
-  50% {
-    transform: translateY(0);
-    animation-timing-function: cubic-bezier(0,0,0.2,1);
-  }
-}
-
-.custom-bounce {
-  animation: my-bounce 1s infinite;
-}
-`}</style>
-
-<i className='custom-bounce fas fa-angle-double-down'></i>
-          
+          <i className='fas fa-angle-double-down bounce' />
           <span>
             {siteConfig('HEXO_SHOW_START_READING', null, CONFIG) &&
               locale.COMMON.START_READING}
