366f8329ed43e0c1fe7980144ecd04c17e4f3a4e
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-06-16 00:45:47 +0800

    Delete themes/dream/components/config.js

--- themes/dream/components/config.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = {
-  // ... existing config
-  BACKGROUND_IMAGES: {
-    light: 'https://cdn.jsdelivr.net/gh/biubiuball/BlogImage/jpg/lightspot.jpeg',
-    dark: 'https://cdn.jsdelivr.net/gh/biubiuball/BlogImage/jpg/nightcity.jpg'
-  }
-};
-
-export default CONFIG;
