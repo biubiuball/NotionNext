0cc7c0b63501c8adaded757629c9cf5cfc11f3b3
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-06-17 03:03:03 +0800

    Update index.js

--- themes/dream/index.js
+++ themes/dream/index.js
@@ -33,7 +33,6 @@ import TocDrawer from './components/TocDrawer'
 import TocDrawerButton from './components/TocDrawerButton'
 import CONFIG from './config'
 import { Style } from './style'
-import CursorFollow from './components/CursorFollow'
 
 const AlgoliaSearchModal = dynamic(
   () => import('@/components/AlgoliaSearchModal'),
@@ -46,6 +45,9 @@ export const useHexoGlobal = () => useContext(ThemeGlobalHexo)
 
 /**
  * 基础布局 采用左右两侧布局，移动端使用顶部导航栏
+ * @param props
+ * @returns {JSX.Element}
+ * @constructor
  */
 const LayoutBase = props => {
   const { post, children, slotTop, className } = props
@@ -76,6 +78,7 @@ const LayoutBase = props => {
         </div>
       )}
       {post && <ButtonJumpToComment />}
+      {showRandomButton && <ButtonRandomPostMini {...props} />}
     </>
   )
 
@@ -88,13 +91,6 @@ const LayoutBase = props => {
         id='theme-hexo'
         className={`${siteConfig('FONT_STYLE')} dark:bg-black scroll-smooth`}>
         <Style />
-
-        {/* 鼠标轨迹效果组件 */}
-        <CursorFollow />
-
-        {/* 恢复背景图片容器 - 修复开始 */}
-        <div className="light-bg fixed inset-0"></div>
-        <div className="dark-bg fixed inset-0"></div>  
 
         {/* 顶部导航 */}
         <Header {...props} />
@@ -116,7 +112,7 @@ const LayoutBase = props => {
         {/* 主区块 */}
         <main
           id='wrapper'
-          className={`${siteConfig('HEXO_HOME_BANNER_ENABLE', null, CONFIG) ? '' : 'pt-16'} bg-transparent w-full py-8 md:px-8 lg:px-24 min-h-screen relative`}>
+          className={`${siteConfig('HEXO_HOME_BANNER_ENABLE', null, CONFIG) ? '' : 'pt-16'} bg-hexo-background-gray dark:bg-black w-full py-8 md:px-8 lg:px-24 min-h-screen relative`}>
           <div
             id='container-inner'
             className={
@@ -168,6 +164,9 @@ const LayoutBase = props => {
 
 /**
  * 首页
+ * 是一个博客列表，嵌入一个Hero大图
+ * @param {*} props
+ * @returns
  */
 const LayoutIndex = props => {
   return <LayoutPostList {...props} className='pt-8' />
@@ -175,6 +174,8 @@ const LayoutIndex = props => {
 
 /**
  * 博客列表
+ * @param {*} props
+ * @returns
  */
 const LayoutPostList = props => {
   return (
@@ -191,6 +192,8 @@ const LayoutPostList = props => {
 
 /**
  * 搜索
+ * @param {*} props
+ * @returns
  */
 const LayoutSearch = props => {
   const { keyword } = props
@@ -230,6 +233,8 @@ const LayoutSearch = props => {
 
 /**
  * 归档
+ * @param {*} props
+ * @returns
  */
 const LayoutArchive = props => {
   const { archivePosts } = props
@@ -252,6 +257,8 @@ const LayoutArchive = props => {
 
 /**
  * 文章详情
+ * @param {*} props
+ * @returns
  */
 const LayoutSlug = props => {
   const { post, lock, validPassword } = props
@@ -318,6 +325,8 @@ const LayoutSlug = props => {
 
 /**
  * 404
+ * @param {*} props
+ * @returns
  */
 const Layout404 = props => {
   const router = useRouter()
@@ -353,6 +362,8 @@ const Layout404 = props => {
 
 /**
  * 分类列表
+ * @param {*} props
+ * @returns
  */
 const LayoutCategoryIndex = props => {
   const { categoryOptions } = props
@@ -389,6 +400,8 @@ const LayoutCategoryIndex = props => {
 
 /**
  * 标签列表
+ * @param {*} props
+ * @returns
  */
 const LayoutTagIndex = props => {
   const { tagOptions } = props
