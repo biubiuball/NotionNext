bc5f7d51caa54fb7d050ec610829260528961e42
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-06-17 04:30:33 +0800

    Update BlogPostCard.js

--- themes/dream/components/BlogPostCard.js
+++ themes/dream/components/BlogPostCard.js
@@ -21,45 +21,46 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
   //   const delay = (index % 2) * 200
 
   return (
+  <div
+    className={`${siteConfig('HEXO_POST_LIST_COVER_HOVER_ENLARGE', null, CONFIG) ? ' hover:scale-110 transition-all duration-150' : ''}`}>
     <div
-      className={`${siteConfig('HEXO_POST_LIST_COVER_HOVER_ENLARGE', null, CONFIG) ? ' hover:scale-110 transition-all duration-150' : ''}`}>
-      <div
-        key={post.id}
-        data-aos='fade-up'
-        data-aos-easing='ease-in-out'
-        data-aos-duration='500'
-        data-aos-once='false'
-        data-aos-anchor-placement='top-bottom'
-        id='blog-post-card'
-        className={`group md:h-56 w-full flex justify-between md:flex-row flex-col-reverse ${siteConfig('HEXO_POST_LIST_IMG_CROSSOVER', null, CONFIG) && index % 2 === 1 ? 'md:flex-row-reverse' : ''}
-                    overflow-hidden border border-transparent rounded-xl bg-white/20 dark:bg-black/20 backdrop-filter backdrop-blur-sm`}
-        {/* 文字内容 */}
-        <BlogPostCardInfo
-          index={index}
-          post={post}
-          showPageCover={showPageCover}
-          showPreview={showPreview}
-          showSummary={showSummary}
-        />
+      key={post.id}
+      data-aos='fade-up'
+      data-aos-easing='ease-in-out'
+      data-aos-duration='500'
+      data-aos-once='false'
+      data-aos-anchor-placement='top-bottom'
+      id='blog-post-card'
+      className={`group md:h-56 w-full flex justify-between md:flex-row flex-col-reverse ${siteConfig('HEXO_POST_LIST_IMG_CROSSOVER', null, CONFIG) && index % 2 === 1 ? 'md:flex-row-reverse' : ''}
+                  overflow-hidden border border-transparent rounded-xl bg-white/20 dark:bg-black/20 backdrop-filter backdrop-blur-sm`}>
+      
+      {/* 文字内容 */}
+      <BlogPostCardInfo
+        index={index}
+        post={post}
+        showPageCover={showPageCover}
+        showPreview={showPreview}
+        showSummary={showSummary}
+      />
 
-        {/* 图片封面 */}
-        {showPageCover && (
-          <div className='md:w-5/12 overflow-hidden'>
-            <Link href={post?.href}>
-              <>
-                <LazyImage
-                  priority={index === 1}
-                  alt={post?.title}
-                  src={post?.pageCoverThumbnail}
-                  className='h-56 w-full object-cover object-center group-hover:scale-110 duration-500'
-                />
-              </>
-            </Link>
-          </div>
-        )}
-      </div>
-    </div>
-  )
+      {/* 图片封面 */}
+      {showPageCover && (
+        <div className='md:w-5/12 overflow-hidden'>
+          <Link href={post?.href}>
+            <>
+              <LazyImage
+                priority={index === 1}
+                alt={post?.title}
+                src={post?.pageCoverThumbnail}
+                className='h-56 w-full object-cover object-center group-hover:scale-110 duration-500'
+              />
+            </>
+          </Link>
+        </div>
+      )}
+    </div> 
+  </div>
+)
 }
 
 export default BlogPostCard
