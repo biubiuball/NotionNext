1b058875a866a0f599c5eb4d8b929ae4baa2f029
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-06-17 13:59:50 +0800

    Update Card.js

--- themes/dream/components/Card.js
+++ themes/dream/components/Card.js
@@ -1,7 +1,10 @@
 const Card = ({ children, headerSlot, className }) => {
   return <div className={className}>
     <>{headerSlot}</>
-    <section className="card shadow-md hover:shadow-md border-transparent rounded-xl lg:p-6 p-4 bg-transparent lg:duration-100">
+   <section className="card shadow-md hover:shadow-xl border border-white/10 rounded-xl lg:p-6 p-4 
+                    bg-white/20 dark:bg-black/20 backdrop-filter backdrop-blur-sm 
+                    hover:backdrop-blur-md backdrop-saturate-150 transition-all duration-300 
+                    lg:duration-100">
         {children}
     </section>
   </div>
