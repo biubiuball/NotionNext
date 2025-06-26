c320181a0ba7a3c36c9f0ae9038e96538f9b606a
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-06-27 03:40:28 +0800

    Update MenuGroupCard.js

--- themes/dream/components/MenuGroupCard.js
+++ themes/dream/components/MenuGroupCard.js
@@ -42,7 +42,7 @@ const MenuGroupCard = props => {
   return (
     <nav
       id='nav'
-      className='leading-8 flex justify-center  dark:text-gray-200 w-full'>
+      className='leading-8 flex justify-center dark:text-gray-200 w-full'>
       {links.map(link => {
         if (link.show) {
           return (
@@ -52,7 +52,7 @@ const MenuGroupCard = props => {
               href={link.href}
               target={link?.target}
               className={
-                'py-1.5 my-1 px-2 duration-300 text-base justify-center items-center cursor-pointer'
+                'py-1 my-0.5 px-2 duration-300 text-base justify-center items-center cursor-pointer'
               }>
               <div className='w-full items-center justify-center hover:scale-105 duration-200 transform dark:hover:text-indigo-400 hover:text-indigo-600'>
                 <div className='text-center'>{link.name}</div>
