0d92a4a2da810ed8df170037163e0d7392429d84
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-06-26 16:56:20 +0800

    Update ButtonJumpToComment.js

--- themes/dream/components/ButtonJumpToComment.js
+++ themes/dream/components/ButtonJumpToComment.js
@@ -1,5 +1,5 @@
-import CONFIG from '../config'
 import { siteConfig } from '@/lib/config'
+import CONFIG from '../config'
 
 /**
  * 跳转到评论区
@@ -8,31 +8,29 @@ import { siteConfig } from '@/lib/config'
  */
 const ButtonJumpToComment = () => {
   if (!siteConfig('HEXO_WIDGET_TO_COMMENT', null, CONFIG)) {
-    return null;
+    return <></>
   }
 
   function navToComment() {
-    const commentElement = document.getElementById('comment');
-    if (commentElement) {
-      const targetPosition = commentElement.offsetTop - headerHeight;
-      window.scrollTo({ 
-        top: targetPosition, 
-        behavior: 'smooth' 
-      });
+    if (document.getElementById('comment')) {
+      window.scrollTo({
+        top: document.getElementById('comment').offsetTop,
+        behavior: 'smooth'
+      })
     }
+    // 兼容性不好
+    // const commentElement = document.getElementById('comment')
+    // if (commentElement) {
+    // commentElement?.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
   }
 
   return (
-    <div 
-      onClick={navToComment}
-      className="cursor-pointer transform hover:scale-105 duration-200"
-    >
-      <i 
-        id="CommentButton"
-        className="fas fa-comments text-white text-sm bg-indigo-700 w-10 h-10 rounded-full dark:bg-indigo-600 flex items-center justify-center" 
-      />
+    <div
+      className="fas fa-comments text-white text-sm bg-indigo-700 w-10 h-10 rounded-full dark:bg-indigo-600 flex items-center justify-center"
+      onClick={navToComment}>
+      <i className='cursor-pointer transform hover:scale-105 duration-200' />
     </div>
-  );
+  )
 }
 
 export default ButtonJumpToComment
