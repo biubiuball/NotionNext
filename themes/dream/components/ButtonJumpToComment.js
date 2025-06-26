b8e72e384a65a1b0d49009e69f41becbf37b6e07
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-06-27 00:05:46 +0800

    Update ButtonJumpToComment.js

--- themes/dream/components/ButtonJumpToComment.js
+++ themes/dream/components/ButtonJumpToComment.js
@@ -20,12 +20,17 @@ const ButtonJumpToComment = () => {
     }
   }
 
-  return <div 
-    className={'justify-center items-center text-center'} onClick={navToComment}>
-        <i className={`fas fa-comments transform hover:scale-105 duration-200 text-white
-         text-sm  bg-indigo-700 w-10 h-10 rounded-full dark:bg-black cursor-pointer py-3`} />
-    </div>
-  )
+  return (
+  <div 
+    className="flex justify-center items-center text-center" 
+    onClick={navToComment}
+  >
+    <i 
+      id="commentButton" 
+      className={`fas fa-comments transform hover:scale-105 duration-200 text-white text-sm bg-indigo-700 w-10 h-10 rounded-full dark:bg-black cursor-pointer flex items-center justify-center`} // 添加了 flex 布局类
+    />
+  </div>
+);
 }
 
 export default ButtonJumpToComment
