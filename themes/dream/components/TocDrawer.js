e6343114222c935a86a162c21fc4974ba2ed0bc4
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-06-27 15:34:45 +0800

    Add files via upload

--- themes/dream/components/TocDrawer.js
+++ themes/dream/components/TocDrawer.js
@@ -21,18 +21,22 @@ const TocDrawer = ({ post, cRef }) => {
   }
   return <>
     <div className='fixed top-0 right-0 z-40 '>
-
       {/* 悬浮目录 */}
       <div
-        className={(showDrawer ? 'animate__slideInRight ' : ' -mr-72 animate__slideOutRight') 
-
+        className={(showDrawer ? 'animate__slideInRight ' : ' -mr-72 animate__slideOutRight') +
+        ' shadow-card animate__animated animate__faster' +
+        ' w-60 duration-200 fixed right-12 bottom-12 rounded py-2 bg-white dark:bg-gray-900'}>
+          {post && <>
+           <div className='dark:text-gray-400 text-gray-600'>
+             <Catalog toc={post.toc}/>
+           </div>
+          </>
           }
       </div>
     </div>
     {/* 背景蒙版 */}
     <div id='right-drawer-background' className={(showDrawer ? 'block' : 'hidden') + ' fixed top-0 left-0 z-30 w-full h-full'}
-
          onClick={switchVisible} />
   </>
 }
-export TocDrawer
+export default TocDrawer
