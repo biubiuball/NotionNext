f4307206f5c5e1bc5ca3e44eee9c6890c3e9f299
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-06-29 00:10:25 +0800

    Update LatestPostsGroup.js

--- themes/dream/components/LatestPostsGroup.js
+++ themes/dream/components/LatestPostsGroup.js
@@ -22,7 +22,7 @@ const LatestPostsGroup = ({ latestPosts, siteInfo }) => {
   return (
     <>
       <div className=' mb-2 px-1 flex flex-nowrap justify-between'>
-        <div>
+        <div className='text-gray-400 dark:text-gray-200'> {/* 亮模式浅灰，暗模式亮白 */}
           <i className='mr-2 fas fas fa-history' />
           {locale.COMMON.LATEST_POSTS}
         </div>
@@ -49,13 +49,14 @@ const LatestPostsGroup = ({ latestPosts, siteInfo }) => {
             </div>
             <div
               className={
-                (selected ? ' text-indigo-400 ' : 'dark:text-gray-400 ') +
+                (selected ? ' text-indigo-400 ' : 'text-gray-400 dark:text-gray-200 ') + 
                 ' text-sm overflow-x-hidden hover:text-indigo-600 px-2 duration-200 w-full rounded ' +
                 ' hover:text-indigo-400 cursor-pointer items-center flex'
               }>
               <div>
                 <div className='line-clamp-2 menu-link'>{post.title}</div>
-                <div className='text-gray-500'>{post.lastEditedDay}</div>
+                {/* 修改这里：调整最后编辑日期的颜色 */}
+                <div className='text-gray-400 dark:text-gray-200'>{post.lastEditedDay}</div>
               </div>
             </div>
           </Link>
