5c082bbe27ec083f2bc7102be711a6f3acb92450
biubiuball <161126041+biubiuball@users.noreply.github.com>
2025-07-21 15:26:08 +0800

    Update BlogPostArchive.js

--- themes/dream/components/BlogPostArchive.js
+++ themes/dream/components/BlogPostArchive.js
@@ -19,26 +19,22 @@ const BlogPostArchive = ({ posts = [], archiveTitle }) => {
           {archiveTitle}
         </div>
         <ul>
-          {posts?.map(post => {
-              <li
-    key={post.id}
-    className='border-l-2 p-1 text-xs md:text-base items-center hover:scale-x-105 hover:border-indigo-500 dark:hover:border-indigo-300 dark:border-indigo-400 transform duration-500'>
-    <div id={post?.publishDay}>
-
-      <span className='text-gray-800 dark:text-white'>{post.date?.start_date}</span>{' '}
-      &nbsp;
-
-      <Link
-        href={post?.href}
-        passHref
-        className='text-gray-800 dark:text-white overflow-x-hidden hover:underline cursor-pointer dark:hover:text-indigo-300'>
-        {post.title}
-      </Link>
-    </div>
-  </li>
-)
-            )
-          })}
+          {posts?.map(post => (
+            <li
+              key={post.id}
+              className='border-l-2 p-1 text-xs md:text-base items-center hover:scale-x-105 hover:border-indigo-500 dark:hover:border-indigo-300 dark:border-indigo-400 transform duration-500'>
+              <div id={post?.publishDay}>
+                <span className='text-gray-800 dark:text-white'>{post.date?.start_date}</span>
+                {' '}&nbsp;
+                <Link
+                  href={post?.href}
+                  passHref
+                  className='text-gray-800 dark:text-white overflow-x-hidden hover:underline cursor-pointer dark:hover:text-indigo-300'>
+                  {post.title}
+                </Link>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     )
